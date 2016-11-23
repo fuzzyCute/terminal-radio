bf809c3b6de99eabbcc6c64076d17687bc37d42f
meninosousa <meninosousa@gmail.com>
2016-11-23 14:25:42 +0100

    py3 faggot
    python 3 master race and other easter eggs

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-__version__ = "0.0.1"
-__author__ = "fuzzyCute"
-__license__ = "OpenSource"
+__version__ = "0.0.over9000_zeroez.1"
+__author__ = "fuzzynotthatCute"
+__license__ = "OpenyourassSource"
 
 import radio_programming as radio, signal
 
 signal.signal(signal.SIGINT, radio.signal_handler)
 
-print radio.spaces
+print (radio.spaces)
 
-print radio.start
+print (radio.start)
 
 while True:
-	user_input = raw_input(radio.user_input_prompt)
+	user_input = input(radio.user_input_prompt)
 	if user_input in radio.start_commands or user_input.startswith("search genre") or user_input.startswith("search radio"):
 		# HELP MENU
 		if user_input == radio.start_commands[3]:
@@ -26,9 +26,9 @@ while True:
 			if genre != "home":
 				id_radio = radio.radios_menu(genre)
 				if id_radio == 0:
-					print radio.spaces
-					print radio.start
-					print "No radios availabre for this genre at the moment, please try another one"
+					print (radio.spaces)
+					print (radio.start)
+					print ("No radios availabre for this genre at the moment, please try another one")
 					
 				elif id_radio != "return_genre":
 					ip = radio.radio_get_ip(id_radio)
@@ -36,12 +36,12 @@ while True:
 				else:
 					id_radio = ""
 					genre = ""
-					print radio.spaces
-					print radio.start
+					print (radio.spaces)
+					print (radio.start)
 			else:
 				genre = ""
-				print radio.spaces
-				print radio.start
+				print (radio.spaces)
+				print (radio.start)
 		
 		# SEARCH BY GENDER OR RADIO MENU
 		elif user_input.startswith("search genre"):
@@ -51,14 +51,14 @@ while True:
 				id_radio = radio.radios_menu(genre)
 				ip = radio.radio_get_ip(id_radio)
 				radio.play_radio(ip)
-				print "teste no radio"
+				print ("teste no radio")
 	
 		elif user_input == radio.start_commands[4]:
-			print "see you around"
+			print ("see you around")
 			radio.sys.exit(0)
 		
 	else:
-		print radio.spaces
-		print radio.start
-		print "unknown Command"
+		print (radio.spaces)
+		print (radio.start)
+		print ("unknown Command")
 		
